dcb064c65873bb6d5c4e7350c91a58154c7e0254
SalviaTh <salviathingbaijam@gmail.com>
2026-03-27 01:12:00 +0530

    added pills

---
+++ script.js
@@ -0,0 +1,11 @@
+const Coloritem=document.querySelectorAll("li");
+const scene=document.getElementById("scene-container");
+Coloritem.forEach(item => {
+    const color=item.textContent;
+    const pill=document.createElement("div");
+    pill.classList.add("pill");
+    pill.textContent=color;
+    pill.style.background=color;
+    scene.appendChild(pill);
+    
+});
